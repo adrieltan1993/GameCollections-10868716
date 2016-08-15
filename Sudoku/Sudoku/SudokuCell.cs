628ed8ea6a48bc68e93c519076b2588c94d50dac
Adriel Tan <adrieltan1993@gmail.com>
2016-08-15 10:20:30 +0800

    Added SudokuCell.cs with getCell() and setCell()

---
+++ Sudoku/Sudoku/SudokuCell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class SudokuCell
+    {
+        int _digit = 0;
+
+        public SudokuCell(int digit, bool isDefault)
+        {
+            _digit = digit;
+        }
+
+        public void setCell(int digit)
+        {
+            _digit = digit;
+        }
+
+        public int getCell()
+        {
+            return _digit;
+        }
+    }
+}
